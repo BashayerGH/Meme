d369184b644a95024e13f50cb9be9eed34a303fa
Bashooora <bashayeralghamdi@Bashayers-MacBook-Pro.local>
2018-12-02 13:34:57 +0300

    Updates

--- Meme/DetailsViewController.swift
+++ Meme/DetailsViewController.swift
@@ -16,11 +16,14 @@ class DetailsViewController: UIViewController {
     @IBOutlet weak var memeimageView: UIImageView!
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         self.memeimageView!.image = meme.memedImage
+        self.tabBarController?.tabBar.isHidden = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        self.tabBarController?.tabBar.isHidden = false
     }
     
 
---
+++ Meme/MemeEditorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemeEditorViewController.swift
+//  Meme
+//
+//  Created by Bashooora on 24/03/1440 AH.
+//  Copyright © 1440 Bashayer. All rights reserved.
+//
+
+import UIKit
+
+class MemeEditorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
